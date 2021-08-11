adf70e73ca1101924e5e4a054d99948a6ffeaabe
Hrishi <Hrishikulkarni143@gmail.com>
2021-08-11 11:47:21 +0530

    First Commit.
    Operations

---
+++ ShriGanesh/src/Basic/Operations.java
@@ -0,0 +1,12 @@
+package Basic;
+import java.io.*;
+import java.util.Scanner;
+public class Operations {
+	public static void main (String[] args)
+	{
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println(c);
+}
+}
