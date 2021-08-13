93d1b8c90b9cfd256479c5c0b72c23c762b16057
Hrishi <Hrishikulkarni143@gmail.com>
2021-08-13 16:17:05 +0530

    Commited B

---
+++ Fast/src/Mid/SomeB.java
@@ -0,0 +1,12 @@
+package Mid;
+import java.io.*;
+import java.util.*;
+public class SomeB {
+
+	public static void main(String[] args) 
+	{
+	 System.out.println("Right Click");
+	 System.out.println("Left Click");
+	}
+
+}
